441f1d7d870a355d7135e5e38a96d68726820c95
Israel Ramirez <nicexreal@gmail.com>
2020-07-14 17:01:56 -0400

    Se crea el método para enviar segmentos de imagenes mediante open mpi

--- img-mang/main.cpp
+++ img-mang/main.cpp
@@ -15,7 +15,7 @@ using namespace cv;
 using namespace std;
 /** Variables globales **/
 float kernel[5][5];
-
+int rangeMin, rangeMax;
 /** Funciones **/
 /**
  * Funcion que guarda la imagen según un formato programa_operacion_yyyymmddHHMMss
@@ -23,7 +23,8 @@ float kernel[5][5];
  * @param operation nombre de la operacion
 */
 void saveImage(Mat image, string operation);
-
+void copyTo(Mat src, Mat dst, int minx, int miny, int maxx, int maxy);
+void join(Mat src, Mat dst,int proceso, int procesadores);
 /** Operacion 1 Difuminado de imagenes **/
 
 /**
@@ -71,31 +72,62 @@ int main(int argc, char** argv ){
         int myrank;
         int tag = 0;
         int procesadores;
-        int rangeMin, rangeMax;
-        Mat img,newimg;
+        string stop("stop");
+        string ready("ready");
+        Mat img, imgsplit, newimg;
         
+        size_t total;
+        size_t elemsize;
+        int sizes[3];
+
+        int LARGO=0;
+        MPI_Status estado;
         MPI_Init(&argc, &argv);
         MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
         MPI_Comm_size(MPI_COMM_WORLD, &procesadores);
-        
+
         if(myrank == 0){
             string path = argv[2];
             img = imread(path,1);
-            newimg = imread(path,1);
+            int mintemp = 0, maxtemp = img.cols/procesadores;
+            rangeMin = 0;
+            rangeMax = img.cols/procesadores;
+            Mat tmpimgsplit(Size(rangeMax,img.rows), img.type());
+            imgsplit = tmpimgsplit.clone();
+            copyTo(img,imgsplit,mintemp,0,maxtemp,img.rows);
+            for(int p = 1; p < procesadores; p++){
+                mintemp = maxtemp;
+                maxtemp += img.cols/procesadores;
+                if(p+1 == procesadores){
+                    maxtemp = img.cols;
+                }
+                int diference = maxtemp-mintemp;
+                Mat imgToSend(Size(diference, img.rows), img.type());
+                copyTo(img,imgToSend,mintemp,0,maxtemp,img.rows);
+                sizes[2]=imgToSend.elemSize();
+                Size s = imgToSend.size();
+                sizes[0] = s.height;
+                sizes[1] = s.width;
+                MPI_Send( sizes, 3, MPI_INT,p,0,MPI_COMM_WORLD);
+                MPI_Send( imgToSend.data, sizes[0]*sizes[1]*3, MPI_CHAR,p,1, MPI_COMM_WORLD);
+            }
+        }
+        else{
+
+            MPI_Recv( sizes,3, MPI_INT,0,0, MPI_COMM_WORLD,&estado);
+            imgsplit.create(sizes[0],sizes[1],CV_8UC3);
+            MPI_Recv( imgsplit.data, sizes[0]*sizes[1]*3, MPI_CHAR,0,1, MPI_COMM_WORLD,&estado);
             
         }
-        
+        newimg = imgsplit.clone();
         if(*argv[1]== '1'){
-            if(myrank == 0){
-                getKernel();
-                for(int i = 0 ; i < img.rows ; i++){
-                    for(int j = 0 ; j < img.cols ; j++){
-                        for(int k = 0; k < 3; k++){
-                            newimg.at<Vec3b>(i,j)[k] = gauss(j,i,img,0,0,img.cols,img.rows,k);
-                        }
+            getKernel();
+            for(int i = 0 ; i < imgsplit.rows ; i++){
+                for(int j = 0 ; j < imgsplit.cols ; j++){
+                    for(int k = 0; k < 3; k++){
+                        newimg.at<Vec3b>(i,j)[k] = gauss(j,i,imgsplit,0,0,imgsplit.cols,imgsplit.rows,k);
                     }
                 }
-                saveImage(newimg,"1");
             }
         }
         else if(*argv[1]== '2'){
@@ -112,6 +144,25 @@ int main(int argc, char** argv ){
             return EXIT_FAILURE;
         }
         
+        if(myrank==0){
+            join(newimg,img,0,procesadores);
+            for(int p = 1; p<procesadores; p++){
+                MPI_Recv( sizes,3, MPI_INT,p,0, MPI_COMM_WORLD,&estado);
+                Mat imgtmpjoin(sizes[0],sizes[1],CV_8UC3);
+                MPI_Recv( imgtmpjoin.data, sizes[0]*sizes[1]*3, MPI_CHAR,p,1, MPI_COMM_WORLD,&estado);
+                join(imgtmpjoin,img,p,procesadores);
+            }
+            saveImage(img,"test");
+        }
+        else{
+            sizes[2]=newimg.elemSize();
+            Size s = newimg.size();
+            sizes[0] = s.height;
+            sizes[1] = s.width;
+            MPI_Send( sizes, 3, MPI_INT,0,0,MPI_COMM_WORLD);
+            MPI_Send( newimg.data, sizes[0]*sizes[1]*3, MPI_CHAR,0,1, MPI_COMM_WORLD);
+        }
+
         MPI_Finalize();
     }
     else{
@@ -130,6 +181,29 @@ void saveImage(Mat image, string operation){
     string date(buf);
     imwrite("/media/compartida/programa_"+operation+"_"+date+".png", image);
 }
+
+void copyTo(Mat src, Mat dst, int minx, int miny, int maxx, int maxy){
+    for(int x = 0; x<maxx-minx; x++){
+        for(int y = 0; y<maxy-miny; y++){
+            dst.at<Vec3b>(y,x)[0] = src.at<Vec3b>(y,x+minx)[0];
+            dst.at<Vec3b>(y,x)[1] = src.at<Vec3b>(y,x+minx)[1];
+            dst.at<Vec3b>(y,x)[2] = src.at<Vec3b>(y,x+minx)[2];
+        }
+    }
+}
+
+void join(Mat src, Mat dst,int proceso, int procesadores){
+    int diferencia = dst.cols/procesadores;
+    diferencia *= proceso;
+    for(int x = 0; x<src.cols; x++){
+        for(int y = 0; y<src.rows; y++){
+            dst.at<Vec3b>(y,diferencia+x)[0] = src.at<Vec3b>(y,x)[0];
+            dst.at<Vec3b>(y,diferencia+x)[1] = src.at<Vec3b>(y,x)[1];
+            dst.at<Vec3b>(y,diferencia+x)[2] = src.at<Vec3b>(y,x)[2];
+        }
+    }
+}
+
 float gauss(int centerx,int centery, Mat image,int minx, int miny, int maxx, int maxy, int channel){
     float gaussBlur = 0;
     for(int x = 0; x<5; x++){
@@ -164,3 +238,4 @@ void RGB2GRAYS(Mat src, Mat dst,int minx, int miny, int maxx, int maxy){
         }
     }
 }
+
